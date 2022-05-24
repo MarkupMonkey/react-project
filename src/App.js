fc44eeac6b7213420d8f251193de49a502b39d8c
Marco Moncada <marco.monkey93@gmail.com>
2022-05-25 00:57:27 +0200

    exercise jsx 03 done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 let hello = <h1>Hello, World!</h1>
 
 function App(name) {
-  name = 'User';
+  // name = 'User'; // if 'name' isn't passed to function nothing will be rendered and console says errors
   return (
     <h1>Hello, {name}</h1>
   );
