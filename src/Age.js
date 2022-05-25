cbd569daf6002c26f32894ea4a9b152d442eda5c
Marco Moncada <marco.monkey93@gmail.com>
2022-05-25 11:40:50 +0200

    exercise consitional-rendering-01

--- src/Age.js
+++ src/Age.js
@@ -2,13 +2,15 @@ import React from "react";
 
 class Age extends React.Component {
     render() {
-        return (
-            <div>
-                {!!this.props.age && <p>Your age is {this.props.age}</p>}
-                {!this.props.age && <p>Your age is unknown</p>}
-            </div>
+        if (this.props.age >= 18) {
+            return (
+                <div>
+                    {<p>Your age is {this.props.age}</p>}
+                </div>
 
-        )
+            )
+        }
+
     }
 }
 
--- src/Welcome.js
+++ src/Welcome.js
@@ -4,10 +4,9 @@ class Welcome extends React.Component {
     render() {
         return (
             <div>
-                
                     {!!this.props.name && <p>Welcome, {this.props.name}!</p>}
                     {!this.props.name && <p>Welcome, user!</p>}
-                    <Age age='28'/>
+                    <Age age={28}/>
             </div>
 
         )
