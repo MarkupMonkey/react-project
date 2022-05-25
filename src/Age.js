07b12172d921d1e28a31815131b12f72dd80d858
Marco Moncada <marco.monkey93@gmail.com>
2022-05-25 11:54:36 +0200

    exercise conditional rendering 03

--- src/Age.js
+++ src/Age.js
@@ -2,12 +2,15 @@ import React from "react";
 
 class Age extends React.Component {
     render() {
-        return (
+        if(this.props.age >= 18 && this.props.age <= 65){
+               return (
             <div>
                 {!!this.props.age && <p>Your age is {this.props.age}</p>}
             </div>
 
         )
+        }
+     
     }
 }
 
