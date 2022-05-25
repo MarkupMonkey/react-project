7fe9a0fb5b0bc761b80a7abb5caf54f8fb6c3d65
Marco Moncada <marco.monkey93@gmail.com>
2022-05-25 02:32:03 +0200

    exercise props 03

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,8 +3,12 @@ class Welcome extends React.Component {
     render() {
         return (
             <div>
-                {!!this.props.name && <p>Welcome, {this.props.name}!</p>}
-                {!this.props.name && <p>Welcome, user!</p>}
+                
+                    {!!this.props.name && <p>Welcome, {this.props.name}!</p>}
+                    {!this.props.name && <p>Welcome, user!</p>}
+                    {!!this.props.age && <p>Your age is {this.props.age}</p>}
+                    {!this.props.age && <p>Your age is unknown</p>}
+                
             </div>
 
         )
