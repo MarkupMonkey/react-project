0b8d3163d19e5d0f0202517cb46c0d0fec3922ed
Marco Moncada <marco.monkey93@gmail.com>
2022-05-26 13:51:21 +0200

    exercise state 05 done

--- src/Counter.js
+++ src/Counter.js
@@ -6,28 +6,33 @@ class Counter extends React.Component {
     }
 
     constructor(props) {
-        super(props) 
+        super(props)
         this.displayCount = <h1>count: {this.state.count}</h1>;
 
         setInterval(() => {
-            this.setState((state) => { // we can pass it an object portion we want update or a callback that recieves the current state and returns the new state
-                return {
-                    count: state.count + (this.props.incrementBy),
-                }
-            })
-            this.displayCount= <h1>count: {this.state.count}</h1>
+            if (this.state.count > 10) {
+                this.setState({ count: this.props.initialValue })
+            } else {
+                this.setState((state) => { // we can pass it an object portion we want update or a callback that recieves the current state and returns the new state
+                    return {
+                        count: state.count + (this.props.incrementBy),
+                    }
+                })
+                this.displayCount = <h1>count: {this.state.count}</h1>
+            }
+
         }, this.props.timeout);
     }
 
-   
+
     render() {
         return (
-            <CounterDisplay count={this.displayCount}/>
+            <CounterDisplay count={this.displayCount} />
         );
     }
 }
 
-Counter.defaultProps={
+Counter.defaultProps = {
     initialValue: 0,
     timeout: 1000,
     incrementBy: 1
