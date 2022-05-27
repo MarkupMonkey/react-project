7bb852f04109758af26f4ff8f3822764617aaf91
Marco Moncada <marco.monkey93@gmail.com>
2022-05-27 21:09:18 +0200

    exercise forms 04 done

--- src/Login.js
+++ src/Login.js
@@ -13,7 +13,7 @@ class Login extends React.Component {
         const name = event.target.name;
         const type = event.target.type;
         const checked = event.target.checked;
-        
+
 
         this.setState({
             [name]: type === 'checkbox' ? checked : value,
@@ -22,27 +22,38 @@ class Login extends React.Component {
 
     onLogin = (event) => {
         this.setState({
-            
+
+        })
+    }
+
+    handlerResetState = (event) => {
+        this.setState({
+            username: '',
+            password: '',
+            remember: false
         })
     }
 
     componentDidUpdate() {
         console.log(this.state)
     }
-    
-    
+
+
 
 
     render() {
         return (
             <>
                 <div>
-                    <button onClick={this.onLogin} disabled={!this.state.username + !this.state.password }>login</button>
+                    <button onClick={this.onLogin} disabled={!this.state.username + !this.state.password}>login</button>
                 </div>
                 <div>
                     <input name="username" value={this.state.username} onChange={this.handlerInputChange} />
                     <input name="password" type="password" value={this.state.password} onChange={this.handlerInputChange} />
                     <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handlerInputChange} />
+                </div>
+                <div>
+                    <button onClick={this.handlerResetState}>reset</button>
                 </div>
 
 
