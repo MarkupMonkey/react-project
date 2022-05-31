7d01a22cdac3a6e9f9f6032583f0f468d17413f1
Marco Moncada <marco.monkey93@gmail.com>
2022-05-31 15:46:37 +0200

    exercise useState 01 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ClickCounter from "./ClickCounter";
+import { ClickCounter } from "./ClickCounter";
 import ClickTracker from "./ClickTracker";
 import Counter from "./Counter";
 import Hello from "./Hello"
@@ -11,52 +11,52 @@ import { TodoList } from "./Todolist";
 import { Container } from "./Container";
 import { LanguageContext } from "./LanguageContext";
 import { DisplayLanguage } from "./DisplayLanguage";
-import {Sum} from "./Sum";
+import { Sum } from "./Sum";
 class Data extends React.Component {
-    state = {
-        language: 'en',
-    }
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
-    render() {
-        return (
-            <>
-                <div>
-                    <select value={this.state.language} onChange={this.handleLanguageChange}>
-                        <option value='en'>English</option>
-                        <option value='it'>Italiano</option>
-                    </select>
-                    <LanguageContext.Provider value={this.state.language}>
-                        <DisplayLanguage />
-                    </LanguageContext.Provider>
-                    <Container title="My exercitation app">
-                        <div className="welcome">
-                            <Welcome name='John' age={18} />
-                        </div>
-                        <Hello />
-                        <Counter />
-                        <ClickCounter />
-                        <ClickTracker />
-                        <InteractiveWelcome />
-                        <Login />
-                        <UncontrolledLogin />
-                        <TodoList>{() => {
+  state = {
+    language: 'en',
+  }
+  handleLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value
+    })
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <select value={this.state.language} onChange={this.handleLanguageChange}>
+            <option value='en'>English</option>
+            <option value='it'>Italiano</option>
+          </select>
+          <LanguageContext.Provider value={this.state.language}>
+            <DisplayLanguage />
+          </LanguageContext.Provider>
+          <Container title="My exercitation app">
+            <div className="welcome">
+              <Welcome name='John' age={18} />
+            </div>
+            <Hello />
+            <Counter />
+            <ClickCounter />
+            <ClickTracker />
+            <InteractiveWelcome />
+            <Login />
+            <UncontrolledLogin />
+            <TodoList>{() => {
 
-                            return (
-                                <div></div>
-                            )
-                        }}
-                        </TodoList>
-                        <Sum />
-                    </Container>
+              return (
+                <div></div>
+              )
+            }}
+            </TodoList>
+            <Sum />
+          </Container>
 
-                </div>
-            </>
-        )
-    }
+        </div>
+      </>
+    )
+  }
 }
 
 export default Data;
--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,28 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import Counterbutton from "./CounterButton";
 
-class ClickCounter extends React.Component {
-    state = {
-        count: 0
+export function ClickCounter({initialValue = 0}) {
+    const [counter, setCounter] = useState(initialValue);
+
+    function handleCounterIncrement() {
+        setCounter(counter => counter + 1)
     };
 
-    handleCounterIncrement = (event) => {
-        console.log(event)
-        this.setState((state) => {
-            return {
-                count: state.count + 1,
-            }
-        })
-    };
-    render() {
-        return (
-            <>
-                <Counterbutton childClick={this.handleCounterIncrement.bind(this)}/>
-                <h1>on click count: {this.state.count}</h1>
-            </>
-
-        )
-    }
+    return (
+        <>
+            <Counterbutton childClick={handleCounterIncrement} />
+            <h1>on click count: {counter}</h1>
+        </>
+    )
 }
-
-export default ClickCounter;
