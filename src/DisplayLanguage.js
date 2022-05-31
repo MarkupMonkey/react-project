fdbd6f915d8b706cf49b174eb7a14a1261726f6a
Marco Moncada <marco.monkey93@gmail.com>
2022-05-31 13:07:00 +0200

    exercise context 02 done

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
 
-export class DisplayLanguage extends React.Component{
-    render(){
-        return(
+let Strings = {
+    en: { SELECTED_LANGUAGE: 'English language choosen' },
+    it: { SELECTED_LANGUAGE: 'Hai scelto lingua italiana' }
+}
+export class DisplayLanguage extends React.Component {
+    render() {
+        return (
             <>
-                
+                <LanguageContext.Consumer>
+                    {language => {
+                        return <div>
+                            <h1>{Strings[language].SELECTED_LANGUAGE}</h1>
+                        </div>
+                    }}
+                </LanguageContext.Consumer>
             </>
         )
     }
