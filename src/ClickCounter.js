97870d2fae92c9f5238f1d94eb708f0ab31beddc
Marco Moncada <marco.monkey93@gmail.com>
2022-06-02 13:15:02 +0200

    exercise useEffect 01 done

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Counterbutton from "./CounterButton";
 
-export function ClickCounter({initialValue = 0}) {
-    const [counter, setCounter] = useState(initialValue);
+export function ClickCounter(props) {
+    const [counter, setCounter] = useState(0);
 
-    function handleCounterIncrement() {
-        setCounter(counter => counter + 1)
+    const handleCounterIncrement = () => {
+        setCounter(state => state + 1)
     };
+
+    const handlerCounterReset = () => {
+        setCounter(0)
+    }
+    useEffect(() => {
+        props.onCounterChange(counter);
+    }, [counter, props])
 
     return (
         <>
             <Counterbutton childClick={handleCounterIncrement} />
+            <button onClick={handlerCounterReset}>Reset</button>
             <h1>on click count: {counter}</h1>
         </>
     )
