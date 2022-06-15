2182ef67b6591abb86d23c4b1ded4dc206b31c14
Marco Moncada <marco.monkey93@gmail.com>
2022-06-15 16:00:20 +0200

    fix: exercise custom hook 04

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,30 +1,22 @@
 import { useState } from 'react';
 
-export const useGithubUser = () => {
-    const [loading, setLoading] = useState(null);
-    const [error, setError] = useState(false);
-    const [userData, setUserData] = useState();
-
-    const fetchUser = async (username) => {
-        setLoading(true);
-        setError(null);
-
-        try {
-            const res = await fetch(`https://api.github.com/users/${username}`);
-            const data = await res.json();
-            setUserData(data);
-        } catch (err) {
-            setError(error);
-            setUserData(null);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return {
-        userData,
-        error,
-        loading,
-        onFetchUser: fetchUser
-    };
-}
+export function useGithubUser() {
+  const [userData, setUserData] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const fetchUser = async (username) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      const data = await res.json();
+      setUserData(data);
+    } catch (err) {
+      setError(error);
+      setUserData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { userData, error, loading, onFetchUser: fetchUser };
+};
