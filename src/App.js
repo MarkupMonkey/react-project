57d4515117767892eda9ac5ed6419e2d786d0acd
Marco Moncada <marco.monkey93@gmail.com>
2022-06-16 20:31:39 +0200

    exercise React Router 01 done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React from "react";
-import { LanguageContext } from "./LanguageContext";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { useState } from "react";
 
+import { Routes, Route } from 'react-router-dom';
+import Welcome from './Welcome';
 export function App() {
 
-    const [language, setLanguage] = useState('en');
-    const handleLanguage = (event) => { setLanguage(event.target.value) };
     return (
         <>
-            <select value={language} onChange={handleLanguage}>
-                <option value='en'>English</option>
-                <option value='it'>Italiano</option>
-            </select>
-            <LanguageContext.Provider value={language}>
-                <DisplayLanguage />
-            </LanguageContext.Provider>
+            <Routes>
+                <Route path='/' element={<Welcome name='Monkey' age={28}/>}/>
+            </Routes>
         </>
     )
 }
