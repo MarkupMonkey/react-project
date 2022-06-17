177bdf4aa41ebd359fce3f1c4b130e0fd274f3f2
Marco Moncada <marco.monkey93@gmail.com>
2022-06-17 11:18:03 +0200

    exercise React Rounter 02 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Welcome from './Welcome';
+import { Container } from './Container';
+import { ClickCounter } from './ClickCounter';
+
 export function App() {
 
     return (
-        <>
+        <Container title='My Awesome App'>
             <Routes>
-                <Route path='/' element={<Welcome name='Monkey' age={28}/>}/>
+                <Route path='/' element={<Welcome name='Monkey' age={28} />} />
+                <Route path='/counter' element={<ClickCounter />} />
             </Routes>
-        </>
+        </Container>
     )
 }
