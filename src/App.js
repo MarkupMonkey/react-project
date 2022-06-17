cbddd9612a0b232f6accc707f120c1ddd00d92c5
Marco Moncada <marco.monkey93@gmail.com>
2022-06-17 13:35:44 +0200

    exercise SWR 02 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { Container } from './Container';
 import GithubUser from './GithubUser';
 
 export function App() {
 
+    const [username, setUsername] = useState();
+    const handleUsername = (event) => {
+        event.preventDefault();
+        setUsername(event.target.elements.username.value);
+    }
     return (
         <Container title='My App'>
-            <GithubUser username='markupmonkey' />
+            <form onSubmit={handleUsername}>
+                <input name='username' />
+                <button type='submit'>Submit</button>
+            </form>
+            <GithubUser username={username} />
         </Container>
     )
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -3,6 +3,6 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function useGithubUser(username) {
-  const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
+  const { data, error } = useSWR(username? `https://api.github.com/users/${username}`: null, fetcher);
   return { data, error, isLoading: !data && !error };
 }
